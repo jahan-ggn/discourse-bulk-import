e228e0edb2a342fc9f986e9817f3abd77b240408
Jahan Gagan <jahanggn9@gmail.com>
2025-05-23 17:23:37 +0530

    Update plugin skeleton with plugin name

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-MyPluginModule::Engine.routes.draw do
+DiscourseBulkImport::Engine.routes.draw do
   get "/examples" => "examples#index"
   # define routes here
 end
 
-Discourse::Application.routes.draw { mount ::MyPluginModule::Engine, at: "my-plugin" }
+Discourse::Application.routes.draw { mount ::DiscourseBulkImport::Engine, at: "discourse-bulk-import" }
